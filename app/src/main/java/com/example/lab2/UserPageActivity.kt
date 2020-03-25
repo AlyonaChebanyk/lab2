6e470bb10b6bf579daa31c4f2bd025e617fd1a4a
Alyona Chebanyk <54353732+AlyonaChebanyk123@users.noreply.github.com>
2020-03-25 16:16:27 +0200

    Initial commit

--- app/src/main/java/com/example/lab2/UserPageActivity.kt
+++ app/src/main/java/com/example/lab2/UserPageActivity.kt
@@ -17,7 +17,7 @@ class UserPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_page)
-
+        
         //display an arrow in toolbar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
